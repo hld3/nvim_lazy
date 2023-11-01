2bc36900c4d01e5995f13d2ea525354d1d7a73ce
Harry Dodson <harry.lee.dodson@gmail.com>
2023-11-01 14:14:40 -0400

    updated jdtls so that it loads on entering a java file, previously a restart was needed for new files.

--- init.lua
+++ init.lua
@@ -410,29 +410,3 @@ cmp.setup {
 		{ name = 'luasnip' },
 	},
 }
-
--- [[ Configure Java jdtls ]]
-local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
-local workspace_dir = "/home/hdodson/.workspace/" .. project_name
--- Don't forget to install jdtls using Mason
-local java_config = {
-	cmd = {
-		'java', -- or '/path/to/java17_or_newer/bin/java'
-		-- depends on if `java` is in your $PATH env variable and if it points to the right version.
-
-		'-Declipse.application=org.eclipse.jdt.ls.core.id1',
-		'-Dosgi.bundles.defaultStartLevel=4',
-		'-Declipse.product=org.eclipse.jdt.ls.core.product',
-		'-Dlog.protocol=true',
-		'-Dlog.level=ALL',
-		'-Xmx1g',
-		'--add-modules=ALL-SYSTEM',
-		'--add-opens', 'java.base/java.util=ALL-UNNAMED',
-		'--add-opens', 'java.base/java.lang=ALL-UNNAMED',
-		'-jar', '/home/hdodson/.jdtls/server/plugins/org.eclipse.equinox.launcher_1.6.500.v20230717-2134.jar',
-		'-configuration', '/home/hdodson/.jdtls/server/config_linux',
-		'-data', workspace_dir,
-	},
-	root_dir = vim.fs.dirname(vim.fs.find({ 'gradlew', '.git', 'mvnw' }, { upward = true })[1]),
-}
-require('jdtls').start_or_attach(java_config)
